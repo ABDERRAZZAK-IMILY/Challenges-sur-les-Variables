6c19d41d082f77af9feee44312f530e2ef4b6dec
abderrazzak imily <imily2024@gmail.com>
2024-09-24 15:17:16 +0100

    commit

---
+++ Calcul et affichage des résultats/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a, b;
+    printf("entre le nombre a : ");
+    scanf("%f", &a);
+    printf("entre le nombre b : ");
+    scanf("%f", &b);
+     printf("a + b = %.2f\n", a + b);
+    printf("a - b = %.2f\n", a - b);
+    printf("a * b = %.2f\n", a * b);
+    printf("a / b = %.2f\n" , a / b);
+
+
+    return 0;
+}
